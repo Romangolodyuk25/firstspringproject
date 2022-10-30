58c2c10a0066e0e5d7e53b66fcbcad7e20844e65
Romangolodyuk25 <112016931+Romangolodyuk25@users.noreply.github.com>
2022-10-30 18:39:39 +0300

    Initial commit

---
+++ src/main/kotlin/person/romchik/firstspringproject/FirstspringprojectApplication.kt
@@ -0,0 +1,11 @@
+package person.romchik.firstspringproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FirstspringprojectApplication
+
+fun main(args: Array<String>) {
+	runApplication<FirstspringprojectApplication>(*args)
+}
---
+++ src/test/kotlin/person/romchik/firstspringproject/FirstspringprojectApplicationTests.kt
@@ -0,0 +1,13 @@
+package person.romchik.firstspringproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FirstspringprojectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
