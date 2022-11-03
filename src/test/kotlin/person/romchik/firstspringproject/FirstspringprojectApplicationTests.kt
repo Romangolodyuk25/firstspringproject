d5a94fbc5cd516130705399756ff918ef9ff3930
Romangolodyuk25 <112016931+Romangolodyuk25@users.noreply.github.com>
2022-11-03 23:18:25 +0300

    Создал класс Car, добавил get'еры и set'еры всем свойствам Создал класс CarService, добавил методы для манипуляций с машинами(addCar, deleteCae, searCar, updateCar) Протестировал все Методы в Test

--- src/test/kotlin/person/romchik/firstspringproject/FirstspringprojectApplicationTests.kt
+++ src/test/kotlin/person/romchik/firstspringproject/FirstspringprojectApplicationTests.kt
@@ -10,4 +10,57 @@ class FirstspringprojectApplicationTests {
 	fun contextLoads() {
 	}
 
+	@Test
+	fun testForMySelf () {
+		var carService = CarService()
+		var firstCar = Car()
+
+		firstCar.brand = "Lada"
+		firstCar.year = 2007
+		firstCar.color = "black"
+
+		var secondCar = Car()
+
+		secondCar.brand = "BMW"
+		secondCar.year = 2010
+
+		var threeCar = Car()
+
+		threeCar.brand = "Mercedes"
+		threeCar.year = 2017
+		threeCar.color = "White"
+		threeCar.isNew = true
+
+		var fourCar = Car()
+
+		fourCar.brand = "Mitsubishi"
+		fourCar.color = "Red"
+		fourCar.year = 2005
+		fourCar.volume = 1.7
+
+		var fiveCar = Car()
+		fiveCar.brand = "Mazda"
+		fiveCar.year = 1999
+		fiveCar.color = "Yellow"
+		fiveCar.isNew = false
+
+
+		carService.addCar(secondCar)
+		carService.addCar(firstCar)
+		carService.addCar(threeCar)
+		carService.addCar(fourCar)
+
+		carService.searchCar(1)
+		carService.update(1, fiveCar)
+		carService.update(2, firstCar)
+		carService.deleteCar(1)
+
+		var result = carService.returnCars()
+
+		for ((key, value) in result) {
+			println(key.toString() + " " + value.brand + " " + value.year + " " + value.color + " " + value.isNew + " " + value.mileage + " " + value.model + " " + value.volume)
+		}
+
+
+	}
 }
