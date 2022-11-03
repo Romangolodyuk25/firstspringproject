0bfa030a4b7692cf57340c38a4407f9a70b28766
Romangolodyuk25 <112016931+Romangolodyuk25@users.noreply.github.com>
2022-11-03 23:19:02 +0300

    Создал класс Car, добавил get'еры и set'еры всем свойствам Создал класс CarService, добавил методы для манипуляций с машинами(addCar, deleteCae, searCar, updateCar) Протестировал все Методы в Test

---
+++ src/main/kotlin/person/romchik/firstspringproject/Car.kt
@@ -0,0 +1,53 @@
+package person.romchik.firstspringproject
+
+class Car() {
+     var brand: String? = null
+        get() {
+            return field
+        }
+        set(value) {
+            field = value
+        }
+     var model: String? = null
+        get () {
+            return field
+        }
+        set(value) {
+            field = value
+        }
+     var year: Int? = null
+        get () {
+            return field
+        }
+        set(value) {
+            field = value
+        }
+     var mileage: Long? = null
+        get() {
+            return field
+        }
+        set(value) {
+            field = value
+        }
+     var isNew: Boolean? = null
+        get() {
+            return field
+        }
+        set(value) {
+            field=value
+        }
+     var color: String? = null
+        get() {
+            return field
+        }
+        set(value) {
+            field = value
+        }
+     var volume: Double? = null
+        get() {
+            return field
+        }
+        set (value) {
+            field = value
+        }
+}
---
+++ src/main/kotlin/person/romchik/firstspringproject/CarService.kt
@@ -0,0 +1,52 @@
+package person.romchik.firstspringproject
+
+import org.springframework.stereotype.Service
+
+//@Service // Помечает класс, как сервисный класс, который можно использовать в этой программе, todo: Раскомментировать
+class CarService {
+    var cars = HashMap<Long, Car>()
+    var idCars = 0L
+
+    /**
+     * Возвращает MAP Со всеми Машинами и их Id
+     */
+    fun returnCars(): Map<Long, Car> {
+        return cars
+    }
+
+    /**
+     * Метод который Добавляет Машину в MAP и добавляет ей Id по счетчику, Передает параметр Car со всеми свойствами
+     */
+    fun addCar(car: Car) {
+        cars.put(idCars,car)
+        idCars++
+    }
+
+    /**
+     * Метод который УДАЛЯЕТ машину по параметру Id из MAP
+     */
+    fun deleteCar(id: Long) {
+        cars.remove(id)
+    }
+
+    /**
+     * Метод который Находит Машину в MAP по парметру Id
+     */
+    fun searchCar(id: Long): Car {
+        return cars.get(id)!!
+    }
+
+    /**
+     * Метод который Находит по Id Машину в MAP и Изменяет ее на другую Машину
+     */
+    fun update(id:Long, car:Car) {
+        cars.get(id)!!.brand = car.brand
+        cars.get(id)!!.year = car.year
+        cars.get(id)!!.color = car.color
+        cars.get(id)!!.model = car.model
+        cars.get(id)!!.isNew = car.isNew
+        cars.get(id)!!.volume = car.volume
+        cars.get(id)!!.mileage = car.mileage
+    }
+
+}
